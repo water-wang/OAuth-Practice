3ed3201eca6b85440840212a5c86c5059edba59d
water-wang <water.wang@0575s.com>
2016-08-29 18:29:19 +0800

    refresh token

--- OAuthAuthentication/OAuthAuthentication.API/Providers/SimpleAuthorizationServerProvider.cs
+++ OAuthAuthentication/OAuthAuthentication.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -1,19 +1,75 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security.OAuth;
+using OAuthAuthentication.API.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using OAuthAuthentication.API.Models.Enums;
 
 namespace OAuthAuthentication.API.Providers
 {
     public class SimpleAuthorizationServerProvider: OAuthAuthorizationServerProvider
     {
-        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+            Client client = null;
+
+            if (!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.TryGetFormCredentials(out clientId, out clientSecret);
+            }
+
+            if (context.ClientId == null)
+            {
+                context.Validated();
+
+                return Task.FromResult<object>(null);
+            }
+
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                client = _repo.FindClient(context.ClientId);
+            }
+
+            if (client == null)
+            {
+                context.SetError("invalid_clientId",
+                    string.Format("Client '{0}' is not registered in the system", context.ClientId));
+            }
+
+            if (client.ApplicationType == ApplicationTypes.NativeConfidential)
+            {
+                if (string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    context.SetError("invalid_clientId", "Client secret should be sent.");
+                    return Task.FromResult<object>(null);
+                }
+                else
+                {
+                    if (client.Secret != Helper.GetHash(clientSecret))
+                    {
+                        context.SetError("invalid_clientId", "Client secret is invalid.");
+                        return Task.FromResult<object>(null);
+                    }
+                }
+            }
+
+            if (!client.Active)
+            {
+                context.SetError("invalid_clientId", "Client is inactive.");
+                return Task.FromResult<object>(null);
+            }
+
+            context.OwinContext.Set("as:clientAllowedOrigin", client.AllowedOrigin);
+            context.OwinContext.Set("as:clientRefreshTokenLifeTime", client.RefreshTokenLifeTime.ToString());
+
             context.Validated();
+            return Task.FromResult<object>(null);
         }
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
