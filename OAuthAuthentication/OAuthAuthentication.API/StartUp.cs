0ba337546d851ce6726be44d52fa56b7bf8d5c06
Walter Wang <water.wang@0575s.com>
2016-08-29 08:19:26 +0800

    OAuth Authentication Back-end API

---
+++ OAuthAuthentication/OAuthAuthentication.API/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using OAuthAuthentication.API.Providers;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(OAuthAuthentication.API.Startup))]
+namespace OAuthAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+            
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ OAuthAuthentication/OAuthAuthentication.Web/app/controllers/homeController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.controller('homeController', ['$scope', function ($scope) {
+    
+}]);
